29d31f4c01590880f4824d2ab765f447c0c44432
Emanoel Silva de Amorim <emanoel.s.amorim@gmail.com>
2026-01-17 03:14:13 +0000

    File changes

--- src/pages/Oportunidades.jsx
+++ src/pages/Oportunidades.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import OportunidadeCard from "@/components/dashboard/OportunidadeCard";
-import { createPageUrl } from "./utils";
+import { createPageUrl } from "@/pages/utils";
 import { 
   Target, 
   Plus, 
